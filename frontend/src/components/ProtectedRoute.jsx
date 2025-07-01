22edc8ed11c82a2cb13787507150f4ea75821c22
Simone2420 <blasssimom@gmail.com>
2025-06-30 19:28:06 -0500

    alpha 0.7

--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -2,11 +2,30 @@ import {Navigate} from 'react-router-dom'
 import {jwtDecode} from 'jwt-decode'
 import api from '../api'
 import { REFRESH_TOKEN, ACCESS_TOKEN } from '../constants'
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
+
 function ProtectedRoute({children}) {
     const [isAuthorized, setIsAuthorized] = useState(null)
+    useEffect(() => {
+        auth().catch(() => setIsAuthorized(false))
+    }, [])
     const refreshToken = async () => {
-        
+        const refreshToken = localStorage.getItem(REFRESH_TOKEN)
+        try{
+            const response = await api.post('/api/token/refresh/', {refresh: refreshToken})
+            if (response.status === 200){
+                localStorage.setItem(ACCESS_TOKEN, response.data.access)
+                setIsAuthorized(true)
+            }
+            else{
+                setIsAuthorized(false)
+
+            }
+        }
+        catch (error) {
+            console.log(error)
+            setIsAuthorized(false)
+        }
     }
     const auth = async () =>{
         const token = localStorage.getItem(ACCESS_TOKEN)
@@ -31,3 +50,4 @@ function ProtectedRoute({children}) {
     }
     return isAuthorized ? children : <Navigate to="login"/>
 }
+export default ProtectedRoute
