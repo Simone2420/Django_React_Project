2394ec8b3b63a0de1f7541632bd38e278cb8855b
Simone2420 <blasssimom@gmail.com>
2025-07-06 08:05:14 -0500

    comit

---
+++ frontend/public/config.js
@@ -0,0 +1,7 @@
+window.configs = {
+    // Para desarrollo local, descomenta esta línea:
+    // apiUrl: 'http://localhost:8000',
+    
+    // Para la API externa de Choreo, usa esta línea:
+    apiUrl: '/choreo-apis/notes-app/backend/v1',
+};
