d9252720d93f6def4eb4bbd654af2746a9ac6ac5
Simone2420 <blasssimom@gmail.com>
2025-07-06 09:51:55 -0500

    mini 6

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -4,7 +4,7 @@ import { ACCESS_TOKEN, REFRESH_TOKEN } from "./constants"
 const apiUrl = "https://note-app-backend-r6bq.onrender.com"
 const api = axios.create({
   baseURL: apiUrl
-  
+  //baseURL: import.meta.env.VITE_API_URL // Uncomment this line if you want to use the environment variable
 })
 
 api.interceptors.request.use(
