b4ca535e9d34efba6163be6633cbaa060b169c12
Simone2420 <blasssimom@gmail.com>
2025-07-06 09:58:02 -0500

    mini 1(2)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,37 +1,39 @@
-import react from 'react'
-import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom'
-import Login from './pages/Login'
-import Register from './pages/Register'
-import NotFound from './pages/NotFound'
-import Home from './pages/Home'
-import ProtectedRoute from './components/ProtectedRoute'
+import react from "react";
+import { HashRouter, Routes, Route, Navigate } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import NotFound from "./pages/NotFound";
+import Home from "./pages/Home";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function Logout() {
   localStorage.clear();
   return <Navigate to="/login" />;
 }
 
-function RegisterAndLogout(){
-  localStorage.clear()
-  return <Register/>
+function RegisterAndLogout() {
+  localStorage.clear();
+  return <Register />;
 }
 function App() {
-
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
-        <Route path="/login" element={<Login/>}/>
-        <Route path="/register" element={<RegisterAndLogout/>}/>
-        <Route path="/logout" element={<Logout/>}/>
-        <Route path="*" element={<NotFound/>}/>
-        <Route path="/" element={
-          <ProtectedRoute>
-            <Home/>
-          </ProtectedRoute>
-        }/>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<RegisterAndLogout />} />
+        <Route path="/logout" element={<Logout />} />
+        <Route path="*" element={<NotFound />} />
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <Home />
+            </ProtectedRoute>
+          }
+        />
       </Routes>
-    </BrowserRouter>
-  )
+    </HashRouter>
+  );
 }
 
-export default App
+export default App;
